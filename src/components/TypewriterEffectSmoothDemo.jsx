0bdff39ce41fbc7ab0bc36ebaf80978a97254b73
Om Chavada <142745673+Om005@users.noreply.github.com>
2025-05-19 21:34:06 +0530

    Update TypewriterEffectSmoothDemo.jsx

--- src/components/TypewriterEffectSmoothDemo.jsx
+++ src/components/TypewriterEffectSmoothDemo.jsx
@@ -27,10 +27,15 @@ const words = [
   const handlebtn = async (e)=>{
     try{
       const data = await dispatch(getFiles(userData.email));
+      window.scrollTo(0, 0);
       navigate('/ide')
     }catch(error){
       toast.error(error.message)
     }
+  }
+  const handleguest = async (e)=>{
+    window.scrollTo(0, 0);
+      navigate('/guest');
   }
   return (
     <div className="flex flex-col items-center justify-center h-[40rem]  ">
@@ -38,7 +43,7 @@ const words = [
       <p className="text-white text-center mb-10">Welcome to PIXELCODE, a flexible code editor for any programming language. Write, save, and manage your code with ease using its intuitive interface and powerful features.</p>
       <div
         className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-        <Link to={'guest'}
+        <Link onClick={handleguest}
           className="w-40 flex justify-center items-center h-12 rounded-xl z-10 bg-black border dark:border-white border-transparent text-white text-lg">
           Explore now
         </Link>

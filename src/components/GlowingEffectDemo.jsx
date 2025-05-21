8b742d4899b460cc79bb96ee6dd0f43cc5d3d241
Om Chavada <142745673+Om005@users.noreply.github.com>
2025-05-21 17:18:54 +0530

    Update GlowingEffectDemo.jsx

--- src/components/GlowingEffectDemo.jsx
+++ src/components/GlowingEffectDemo.jsx
@@ -12,22 +12,22 @@ export function GlowingEffectDemo() {
         description="Seamlessly execute code in over 30+ languages with blazing-fast performance." />
       <GridItem
         area="md:[grid-area:1/7/2/13] xl:[grid-area:2/1/3/5]"
-        icon={<Settings className="h-4 w-4 text-black dark:text-neutral-400" />}
+        icon={<Settings className="h-4 w-4 text-neutral-400" />}
         title="Multiple Themes"
         description="Choose from vibrant themes to create your perfect coding atmosphere." />
       <GridItem
         area="md:[grid-area:2/1/3/7] xl:[grid-area:1/5/3/8]"
-        icon={<Lock className="h-4 w-4 text-black dark:text-neutral-400" />}
+        icon={<Lock className="h-4 w-4 text-neutral-400" />}
         title="Guest & Authenticated Access"
         description="Explore as a guest or unlock premium features as an authenticated user." />
       <GridItem
         area="md:[grid-area:2/7/3/13] xl:[grid-area:1/8/2/13]"
-        icon={<Sparkles className="h-4 w-4 text-black dark:text-neutral-400" />}
+        icon={<Sparkles className="h-4 w-4 text-neutral-400" />}
         title="AI Chatbot Support"
         description="Chat with an AI-powered assistant to resolve your coding doubts instantly." />
       <GridItem
         area="md:[grid-area:3/1/4/13] xl:[grid-area:2/8/3/13]"
-        icon={<Search className="h-4 w-4 text-black dark:text-neutral-400" />}
+        icon={<Search className="h-4 w-4 text-neutral-400" />}
         title="Flexible Input Options"
         description="Upload files, drag and drop, or manually input text with ease." />
     </ul>
@@ -50,18 +50,18 @@ const GridItem = ({
           proximity={64}
           inactiveZone={0.01} />
         <div
-          className="border-0.75 relative flex h-full flex-col justify-between gap-6 overflow-hidden rounded-xl p-6 md:p-6 dark:shadow-[0px_0px_27px_0px_#2D2D2D]">
+          className="border-0.75 relative flex h-full flex-col justify-between gap-6 overflow-hidden rounded-xl p-6 md:p-6 shadow-[0px_0px_27px_0px_#2D2D2D]">
           <div className="relative flex flex-1 flex-col justify-between gap-3">
             <div className="w-fit rounded-lg border border-gray-600 p-2">
               {icon}
             </div>
             <div className="space-y-3">
               <h3
-                className="-tracking-4 pt-0.5 font-sans text-xl/[1.375rem] font-semibold text-balance text-black md:text-2xl/[1.875rem] dark:text-white">
+                className="-tracking-4 pt-0.5 font-sans text-xl/[1.375rem] font-semibold text-balance md:text-2xl/[1.875rem] text-white">
                 {title}
               </h3>
               <h2
-                className="font-sans text-sm/[1.125rem] text-black md:text-base/[1.375rem] dark:text-neutral-400 [&_b]:md:font-semibold [&_strong]:md:font-semibold">
+                className="font-sans text-sm/[1.125rem] md:text-base/[1.375rem] text-neutral-400 [&_b]:md:font-semibold [&_strong]:md:font-semibold">
                 {description}
               </h2>
             </div>

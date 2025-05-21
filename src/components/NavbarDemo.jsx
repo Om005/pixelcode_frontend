bd8efc2c681ed84121364f505d61bb9ebf806e5b
Om Chavada <142745673+Om005@users.noreply.github.com>
2025-05-21 16:52:43 +0530

    Update NavbarDemo.jsx

--- src/components/NavbarDemo.jsx
+++ src/components/NavbarDemo.jsx
@@ -108,7 +108,7 @@ export function NavbarDemo() {
                 key={`mobile-link-${idx}`}
                 to={item.link}
                 onClick={() => setIsMobileMenuOpen(false)}
-                className="relative text-neutral-600 dark:text-neutral-300">
+                className="relative text-neutral-300">
                 <span className="block">{item.name}</span>
               </Link>
             ))}
